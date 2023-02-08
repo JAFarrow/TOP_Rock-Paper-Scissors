7f1234894700f37446f92a097df6e6a735e751cd
Justin <f.justin@tutanota.com>
2023-02-08 22:26:21 +0200

    Implemented RNG system for computer choice, helper function for user input, logic for a single round

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let rng = Math.floor(Math.random() * 3);
+    if (rng == 1) {
+        return "rock";
+    } else if (rng == 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function playerSelection() {
+   let selection = prompt("Choose your weapon!");
+   return selection;
+}
+
+function gameRound(playerSelection, computerSelection) {
+    console.log(computerSelection)
+    if (playerSelection == computerSelection) {
+        return "Tie!";
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection != "scissors") {
+        return "You lose..";
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection != "rock") {
+        return "You lose..";
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection != "paper") {
+        return "You lose.."
+    } else {
+        return "You win!"
+    }
+}
+
+console.log(gameRound(playerSelection(), getComputerChoice()))

2f9e025d4bfe6af772eceaed7a44f281a8b09e4f
Justin <f.justin@tutanota.com>
2023-02-16 22:52:41 +0200

    linked game logic to user input through buttons, added slection icoms, winner is displayed by a change of colour in field

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const playerField = document.querySelector('.playerField');
+const compField = document.querySelector('.compField');
+const rockSelec = document.querySelector('#rock')
+const paperSelec = document.querySelector('#paper')
+const scissorsSelec = document.querySelector('#scissors')
+
 function getComputerChoice() {
     let rng = Math.floor(Math.random() * 3);
     if (rng == 1) {
@@ -10,46 +16,50 @@ function getComputerChoice() {
 }
 
 function gameRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Choose your weapon!");
-    computerSelection = getComputerChoice();
     if (playerSelection == computerSelection) {
-        return "Tie!";
-    } else if (playerSelection.toLowerCase() == "rock" && computerSelection != "scissors") {
-        return "You lose..";
-    } else if (playerSelection.toLowerCase() == "paper" && computerSelection != "rock") {
-        return "You lose..";
-    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection != "paper") {
-        return "You lose.."
-    } else if (playerSelection.toLowerCase() != "rock" && playerSelection.toLowerCase() != "paper" && playerSelection.toLowerCase() != "scissors") {
-        return "Please select a valid weapon!"
+        return 'tie';
+    } else if (playerSelection == "rock" && computerSelection != "scissors") {
+        return 'loss';
+    } else if (playerSelection == "paper" && computerSelection != "rock") {
+        return 'loss';
+    } else if (playerSelection == "scissors" && computerSelection != "paper") {
+        return 'loss';
     } else {
-        return "You win!"
+        return 'win'
     }
 }
 
-function game() {
-    let playerFinalScore = 0;
-    let computerFinalScore = 0;
-    for (let i = 0; i < 5; i++) {
-      gameResult = gameRound();
-      console.log(gameResult)
-        if (gameResult == "You win!") {
-          playerFinalScore++;
-        } else if (gameResult == "You lose..") {
-          computerFinalScore++
-        }
-      console.log("Player Score = " + playerFinalScore)
-      console.log("Computer Score = " + computerFinalScore)
+function fieldManipulator (result) {
+    playerField.style.backgroundColor = '#e0ddd5';
+    compField.style.backgroundColor = '#e0ddd5';
+    if (result == 'tie') {
+        console.log('tie');
+        playerField.style.backgroundColor = '#283d70';
+        compField.style.backgroundColor = '#c6934b';
+    } else if (result == 'win') {
+        console.log('win');
+        playerField.style.backgroundColor = '#283d70';
+    } else if (result == 'loss') {
+        console.log('loss');
+        compField.style.backgroundColor = '#c6934b';
     }
-    scoreReporter(playerFinalScore, computerFinalScore)
 }
 
-function scoreReporter(playerScore, computerScore) {
-    if (playerScore > computerScore) {
-        console.log('You win the game!')
-    } else if (playerScore < computerScore) {
-        console.log("You lost the game!")
-    } else {
-        console.log("The game is a tie!")
-    }
+function fullGame (selection) {
+    result = gameRound(selection, getComputerChoice());
+    fieldManipulator(result);
 }
+
+
+
+rockSelec.addEventListener('click', () => {
+    fullGame('rock');
+});
+
+paperSelec.addEventListener('click', () => {
+    fullGame('paper');
+});
+
+scissorsSelec.addEventListener('click', () => {
+    fullGame('scissors');
+});

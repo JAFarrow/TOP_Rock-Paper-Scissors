123eeb1b06a295af96e31580edbf36b78a765da9
Justin <f.justin@tutanota.com>
2023-02-18 10:05:12 +0200

    Game fully functional, added textual context, tally system, secret, page reset.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let secret = 0;
+const resetBtn = document.querySelector('.reset')
+const secretClick = document.querySelector('.secret');
 const playerField = document.querySelector('.playerField');
 const compField = document.querySelector('.compField');
-const rockSelec = document.querySelector('#rock')
-const paperSelec = document.querySelector('#paper')
-const scissorsSelec = document.querySelector('#scissors')
+const conField = document.querySelector('.contextBox');
+const rockSelec = document.querySelector('#rock');
+const paperSelec = document.querySelector('#paper');
+const scissorsSelec = document.querySelector('#scissors');
+const playerTally = document.querySelector('.playerTally');
+const compTally = document.querySelector('.compTally');
 
 function getComputerChoice() {
     let rng = Math.floor(Math.random() * 3);
@@ -16,7 +22,9 @@ function getComputerChoice() {
 }
 
 function gameRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
+    if (secret == 1) {
+        return 'winAlways';
+    } else if (playerSelection == computerSelection) {
         return 'tie';
     } else if (playerSelection == "rock" && computerSelection != "scissors") {
         return 'loss';
@@ -29,25 +37,50 @@ function gameRound(playerSelection, computerSelection) {
     }
 }
 
-function fieldManipulator (result) {
+function fieldManipulator(result) {
     playerField.style.backgroundColor = '#e0ddd5';
     compField.style.backgroundColor = '#e0ddd5';
     if (result == 'tie') {
-        console.log('tie');
+        conField.innerHTML = '<h4>Player and Computer Tie!</h4>';
         playerField.style.backgroundColor = '#283d70';
         compField.style.backgroundColor = '#c6934b';
     } else if (result == 'win') {
-        console.log('win');
+        conField.innerHTML = '<h4>You Won!</h4>';
         playerField.style.backgroundColor = '#283d70';
+        playerScore++;
+        playerTally.innerHTML = '<h1>' + playerScore + '</h1';
     } else if (result == 'loss') {
-        console.log('loss');
+        conField.innerHTML = '<h4>You Lost!</h4>';
         compField.style.backgroundColor = '#c6934b';
+        compScore++;
+        compTally.innerHTML = '<h1>' + compScore + '</h1'
+    } else if (result == 'winAlways') {
+        conField.innerHTML = '<h4>You Won! Cheater..</h4>';
+        playerField.style.backgroundColor = '#283d70';
+        playerScore++;
+        playerTally.innerHTML = '<h1>' + playerScore + '</h1';
     }
 }
 
-function fullGame (selection) {
+let playerScore = 0;
+let compScore = 0;
+
+function fullGame(selection) {
     result = gameRound(selection, getComputerChoice());
-    fieldManipulator(result);
+    if (playerScore < 5 && compScore < 5) {
+        fieldManipulator(result);
+    }
+}
+
+function reset() {
+    playerField.style.backgroundColor = '#e0ddd5';
+    compField.style.backgroundColor = '#e0ddd5';
+    playerScore = 0;
+    compScore = 0;
+    secret = 0;
+    conField.innerHTML = ''
+    playerTally.innerHTML = ''
+    compTally.innerHTML = ''
 }
 
 
@@ -63,3 +96,12 @@ paperSelec.addEventListener('click', () => {
 scissorsSelec.addEventListener('click', () => {
     fullGame('scissors');
 });
+
+secretClick.addEventListener('click', () => {
+    secret = 1;
+    conField.innerHTML = '<h4>Nice Find..</h4>';
+});
+
+resetBtn.addEventListener('click', () => {
+    reset();
+}) 
